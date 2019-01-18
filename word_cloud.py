083e68db8c50f83a1b5de79d756ae726b16f8503
Fatemeh Kiaie <40069260+fatemehKi@users.noreply.github.com>
2019-01-18 11:34:49 -0500

    Update word_cloud.py

--- word_cloud.py
+++ word_cloud.py
@@ -1,40 +1,39 @@
-# Python 3
-
-# Be sure you have followed the instructions to download the 98-0.txt,
-# the text of A Tale of Two Cities, by Charles Dickens
+# author fatemeh
 
 import collections
 
-file=open('98-0.txt')
+file="""" 
+bands which have connected them with another, and to assume among the
+powers of the earth, the separate and equal station to which the Laws
+of Nature and of Nature's God entitle them, a decent respect to the
+opinions of mankind requires that they should declare the causes which
+impel them to the separation.  We hold these truths to be
+self-evident, that all men are created equal, that they are endowed by
+their Creator with certain unalienable Rights, that among these are
+Life, Liberty and the pursuit of Happiness.--That to secure these
+rights, Governments are instituted among Men, deriving their just
+powers from the consent of the governed, --That whenever any Form of
+Government becomes destructive of these ends, it is the Right of the
+People to alter or to abolish it, and to institute new Government,
+laying its foundation on such principles and organizing its powers in
+such form, as to them shall seem most likely to effect their Safety
+and Happiness. """
 
-# if you want to use stopwords, here's an example of how to do this
-# stopwords = set(line.strip() for line in open('stopwords'))
+# Cleaning text and lower casing all words
+for char in '-.,\n':
+    file=file.replace(char,' ')
+file = file.lower()
 
-# create your data structure here.  F
-wordcount={}
+#splitting the file into the words
+f_wordlist = file.split()
 
-# Instantiate a dictionary, and for every word in the file, add to 
-# the dictionary if it doesn't exist. If it does, increase the count.
-
-# Hint: To eliminate duplicates, remember to split by punctuation, 
-# and use case demiliters. The functions lower() and split() will be useful!
-
-for word in file.read().lower().split():
-    word = word.replace(".","")
-    word = word.replace(",","")
-    word = word.replace("\"","")
-    word = word.replace("“","")
-    if word not in stopwords:
-        if word not in wordcount:
-            wordcount[word] = 1
-        else:
-            wordcount[word] += 1
-
-# after building your wordcount, you can then sort it and return the first
-# n words.  If you want, collections.Counter may be useful.
-
-d = collections.Counter(wordcount)
-
-#print(d.most_common(10))
-for word, count in d.most_common(10):
-	print(word, ": ", count)
+# initializing/creating a dictionary to keep record of the words and the number of those
+dic = {}
+for i in f_wordlist:    
+    dic[i] =dic.get(i,0)+1
+    
+#sorting the list based on the number of repeatation
+sorted_list = []
+for wo, fre in dic.items():
+    sorted_list.append((fre,wo))
+sorted_list.sort(reverse=True)
